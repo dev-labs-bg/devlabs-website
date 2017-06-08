f5af213dbc0766bf36013f68ebec622c7c383989
superKalo <superkalo@devlabs.bg>
2017-06-08 17:13:12 +0300

    Add: configure a basic automation with gulp + elixir; Add: task for minifying images Add: move the original images in the img-uncompressed folder

---
+++ gulpfile.js
@@ -0,0 +1,23 @@
+var elixir = require('laravel-elixir');
+
+require('laravel-elixir-imagemin');
+
+elixir.config.images = {
+    folder: 'img-uncompressed',
+    outputFolder: 'img'
+};
+
+elixir(function(mix) {
+    /**
+     * Minify images with ImageMin: https://github.com/imagemin/imagemin
+     *  - gifsicle — Compress GIF images
+     *  - jpegtran — Compress JPEG images
+     *  - optipng — Compress PNG images
+     *  - svgo — Compress SVG images
+     * Use a Laravel Elixir wrapper for ImageMin
+     * https://github.com/nathanmac/laravel-elixir-imagemin
+     */
+    mix.imagemin({
+        optimizationLevel: 3, progressive: true, interlaced: true
+    });
+});
